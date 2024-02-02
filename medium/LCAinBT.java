4ea1437d263f2c3adf44344f77c709f36bc28473
Chaitanya7725 <chaitanya7725@gmail.com>
2024-02-01 22:26:45 -0600

    Improved Tree Data structure.

--- medium/LCAinBT.java
+++ medium/LCAinBT.java
@@ -2,43 +2,29 @@ package medium;
 
 import com.sun.source.tree.Tree;
 
-class TreeNode1 {
-    int val;
-    TreeNode1 left;
-    TreeNode1 right;
-
-    TreeNode1() {}
-
-    TreeNode1(int val) {
-        this.val = val;
-    }
-    TreeNode1(int val, TreeNode1 left, TreeNode1 right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 public class LCAinBT {
 
     public static void main(String[] args) {
-        TreeNode1 left = new TreeNode1(5);
-        TreeNode1 right = new TreeNode1(1);
-        TreeNode1 root = new TreeNode1(3, left, right);
-        left.left = new TreeNode1(6);
-        left.right = new TreeNode1(2, new TreeNode1(7), new TreeNode1(4));
-        right.left = new TreeNode1(9);
-        right.right = new TreeNode1(8);
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
 
-        TreeNode1 p = new TreeNode1(5);
-        TreeNode1 q = new TreeNode1(4);
-        System.out.println(lowestCommonAncestor(root, p, q).val);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(8);
+
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        System.out.println(lowestCommonAncestor(root, new TreeNode(5), new TreeNode(4)));
     }
 
-    public static TreeNode1 lowestCommonAncestor(TreeNode1 root,TreeNode1 p, TreeNode1 q) {
+    public static TreeNode lowestCommonAncestor(TreeNode root,TreeNode p, TreeNode q) {
         if (root ==null) return null;
         if (root.val == p.val || root.val == q.val) return root;
-        TreeNode1 left = lowestCommonAncestor(root.left, p, q);
-        TreeNode1 right = lowestCommonAncestor(root.right, p, q);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
         if(left != null &&  right != null) return root;
         return left != null? left: right;
