b2f4f187330537d4b397402653049c82ffc111b0
Chaitanya7725 <chaitanya7725@gmail.com>
2024-02-02 21:10:10 -0600

    Pre, In, Post-Traversal Techniques in Binary Tree

---
+++ easy/BinaryTreePostorderTraversal.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(inorderTraversal(root));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public static void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            helper(root.left, res);
+            helper(root.right, res);
+            res.add(root.val);
+        }
+    }
+
+}
---
+++ easy/BinaryTreePreorderTraversal.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(inorderTraversal(root));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public static void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            res.add(root.val);
+            helper(root.left, res);
+            helper(root.right, res);
+        }
+    }
+
+}
