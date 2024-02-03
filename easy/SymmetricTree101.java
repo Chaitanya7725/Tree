3bcfbf4cf02b848089e40b1b400405e5ae9bddf9
Chaitanya7725 <chaitanya7725@gmail.com>
2024-02-03 01:16:55 -0600

    ANDing root value after checking with the recursive call with its childs left and right as arguments

---
+++ easy/SymmetricTree101.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class SymmetricTree101 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(4);
+        System.out.println(isSymmetric(root));
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        return check(root,root);
+    }
+
+    public static boolean check(TreeNode p,TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+
+        return p.val == q.val && check(p.left,q.right);
+    }
+
+}
